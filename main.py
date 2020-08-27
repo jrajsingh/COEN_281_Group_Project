0cace1c6a83f0f2d45c5813dc956485f7861fe5f
jrajsingh <36123005+jrajsingh@users.noreply.github.com>
2020-08-27 15:44:56 +0800

    Changing to perform wafer level modeling

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import nil
 import pandas as pd
 import seaborn as sns
 import time
 
 from os import listdir
-from sklearn import preprocessing
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
 
 
 def forest_test(X, Y):
-    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.30, random_state=101)
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.30, random_state=51)
     start = time.process_time()
-    trained_forest = RandomForestClassifier(n_estimators=700).fit(x_train, y_train)
+    trained_forest = RandomForestClassifier(n_estimators=1000).fit(x_train, y_train)
     print(time.process_time() - start)
     prediction_forest = trained_forest.predict(x_test)
     print(confusion_matrix(y_test, prediction_forest))
@@ -29,13 +25,12 @@ def read_data_from_directory(wafer_class, machine_step):
     df = pd.DataFrame()
     dir_content = listdir("../Wafer_Data/" + wafer_class + "/" + machine_step + "/")
     dir_content.sort()
-    cnt = 0
     for file in dir_content:
         filepath = "../Wafer_Data/" + wafer_class + "/" + machine_step + "/" + file
         print("Reading File {0}".format(file))
         df = df.append(pd.read_csv(filepath))
 
-    wafer_class_bool = nil
+    df = df.groupby(['WaferID', "STEP ID"]).describe(percentiles=[])
     if 'good' in wafer_class:
         df.insert(loc=len(df.columns), column="CLASS", value='Good')
     elif 'bad' in wafer_class:
@@ -43,23 +38,94 @@ def read_data_from_directory(wafer_class, machine_step):
     return df
 
 
-# reading all Z1 Data
-good_z1_df = read_data_from_directory("good_wafer", "Z1_100")
-bad_z1_df = read_data_from_directory("bad_wafer", "Z1_100")
+def remove_columns(df):
+    # Removing "COUNT" Column
+    col_index = len(df.columns) - 1 - 6
+    while col_index >= 0:
+        print('Removing Column Number: ', col_index)
+        print(df.columns[col_index])
+        df.drop(df.columns[col_index], axis=1, inplace=True)
+        col_index = col_index - 6
 
-ndf = good_z1_df.drop(['TIME', 'STEP ID', 'VEL', 'ACC', 'CLASS'], axis=1)
-plt.figure(figsize=(20, 20))
-sns.heatmap(ndf.corr(), annot=True)
-plt.show()
+    # Removing 50% Columns
+    col_index = len(df.columns) - 1 - 2
+    while col_index >= 0:
+        print('Removing Columns Number: ', col_index)
+        #     df.drop(df.columns[col_index], axis=1, inplace=True)
+        print(df.columns[col_index])
+        col_index = col_index - 5
 
-df = pd.DataFrame(good_z1_df.append(bad_z1_df), columns=good_z1_df.columns)
 
-x = df.dropna().reset_index().iloc[:, :-5]
-y = df.dropna().reset_index().iloc[:, -1]
+def run_z1():
+    # reading all Z1 Data
+    good_z1_df = read_data_from_directory("good_wafer", "Z1_100")
+    bad_z1_df = read_data_from_directory("bad_wafer", "Z1_100")
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30, random_state=101)
-trained_forest = RandomForestClassifier(n_estimators=300).fit(x_train, y_train)
+    # Creating combined dataset of both good and bad
+    df = pd.DataFrame(good_z1_df.append(bad_z1_df), columns=good_z1_df.columns)
 
-prediction_forest = trained_forest.predict(x_test)
-print(confusion_matrix(y_test, prediction_forest))
-print(classification_report(y_test, prediction_forest))
+    # Remove Unnecessary Columns
+    remove_columns(df)
+
+    # Configuring the Heatmap to make it easier to see
+    plt.figure(figsize=(30, 30))
+    sns.heatmap(df.corr(), annot=True)
+    plt.show()
+
+    # Dividing into Inputs and Outputs and run Random Forest Classification
+    x_df = df.iloc[:, :-1]
+    y_df = df.iloc[:, -1]
+    x_df = x_df.fillna(0)
+    forest_test(x_df, y_df)
+
+
+def run_z2():
+    # reading all Z1 Data
+    good_z2_df = read_data_from_directory("good_wafer", "Z2_100")
+    bad_z2_df = read_data_from_directory("bad_wafer", "Z2_100")
+
+    # Creating combined dataset of both good and bad
+    df = pd.DataFrame(good_z2_df.append(bad_z2_df), columns=good_z2_df.columns)
+
+    # Remove Unnecessary Columns
+    remove_columns(df)
+
+    # Configuring the Heatmap to make it easier to see
+    plt.figure(figsize=(30, 30))
+    sns.heatmap(round(df.corr(), 2), annot=True)
+    plt.show()
+
+    # Dividing into Inputs and Outputs and run Random Forest Classification
+    x_df = df.iloc[:, :-1]
+    y_df = df.iloc[:, -1]
+    x_df = x_df.fillna(0)
+    forest_test(x_df, y_df)
+
+
+def run_z3():
+    # reading all Z1 Data
+    good_z2_df = read_data_from_directory("good_wafer", "Z3_100")
+    bad_z2_df = read_data_from_directory("bad_wafer", "Z3_100")
+
+    # Creating combined dataset of both good and bad
+    df = pd.DataFrame(good_z2_df.append(bad_z2_df), columns=good_z2_df.columns)
+
+    # Remove Unnecessary Columns
+    remove_columns(df)
+
+    # Configuring the Heatmap to make it easier to see
+    plt.figure(figsize=(30, 30))
+    sns.heatmap(round(df.corr(), 2), annot=True)
+    plt.show()
+
+    # Dividing into Inputs and Outputs and run Random Forest Classification
+    x_df = df.iloc[:, :-1]
+    y_df = df.iloc[:, -1]
+    x_df = x_df.fillna(0)
+    forest_test(x_df, y_df)
+
+
+if __name__ == "__main__":
+    run_z1()
+    run_z2()
+    run_z3()
